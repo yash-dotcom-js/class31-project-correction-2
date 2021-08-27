3e0f428de9acdd1f0a5da805bcce0a2cf22d7e16
yash-dotcom-js <83535992+yash-dotcom-js@users.noreply.github.com>
2021-08-27 15:13:31 +0100

    Add files via upload
    work for class 31 project. fro whj only:( Sincerely made by Yash

---
+++ Block.js
@@ -0,0 +1,38 @@
+class Block {
+    constructor(x,y,w,h){
+        var options = {
+           
+            restitution :0.4,
+            friction :0.0,
+            //isStatic:true
+           
+        }
+            this.body= Bodies.rectangle(x,y,w,h,options);
+            World.add(world,this.body);
+            this.w = w;
+            this.h = h;
+            this.visibility=255;
+    }
+    display(){
+        if(this.body.speed<5){
+
+
+        
+        push();
+        var angle = this.body.angle;
+        var pos= this.body.position;
+        translate(pos.x, pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        rect(0,0,this.w, this.h);
+        pop();
+    }
+    else{
+        World.remove(world, this.body);
+        push();
+        this.Visiblity = this.Visiblity - 5;
+        tint(255,this.Visiblity);
+        pop();
+      }
+
+    }}
